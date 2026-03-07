20536f5ee8c8d34bc8baad472441e9dbb5874ed0
JoshuaRazon10 <joshuarazon10@gmail.com>
2026-03-07 12:58:43 +0800

    feat(frontend): update base path and package scripts for production deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/front_end_se2_lab6/' // Updated base path
+  base: './', // Gamitin ang './' para maging relative ang lahat ng paths
 })
